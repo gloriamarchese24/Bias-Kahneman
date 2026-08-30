07609e9e890c7fed74a996a9117f55d18c9d84c1
gloria <gloria>
2026-08-31 00:52:23 +0200

    Update Schwarz et al. (1991) Availability Heuristic text to 'Please recall 6/12 situations' / 'Richiama alla memoria 6/12 situazioni'

--- pages/8_Euristica_Disponibilita_EN.py
+++ pages/8_Euristica_Disponibilita_EN.py
@@ -60,15 +60,15 @@ st.markdown("""<p class="exp-subtitle">Please answer the questions below</p>""",
 if not st.session_state[NOME_ESPERIMENTO]:
 
     if st.session_state.gruppo == "A":
-        st.markdown("""1. Recall and list **6 situations** in which you managed to behave very assertively (standing up firmly for your point of view).""")
-        st.text_area('Briefly list the 6 situations:', height=140, key='t1')
+        st.markdown("""1. Please recall **6 situations** in which you managed to behave very assertively (standing up firmly for your point of view).""")
+        st.text_area('Briefly describe the 6 situations:', height=140, key='t1')
         st.markdown("""---""")
         st.markdown("""**In general, how assertive do you consider yourself to be (1-10)?**""")
         val = st.radio('', [1,2,3,4,5,6,7,8,9,10], horizontal=True, index=None, key='s1')
 
     else:
-        st.markdown("""1. Recall and list **12 situations** in which you managed to behave very assertively (standing up firmly for your point of view).""")
-        st.text_area('Briefly list the 12 situations:', height=200, key='t2')
+        st.markdown("""1. Please recall **12 situations** in which you managed to behave very assertively (standing up firmly for your point of view).""")
+        st.text_area('Briefly describe the 12 situations:', height=200, key='t2')
         st.markdown("""---""")
         st.markdown("""**In general, how assertive do you consider yourself to be (1-10)?**""")
         val = st.radio('', [1,2,3,4,5,6,7,8,9,10], horizontal=True, index=None, key='s2')
